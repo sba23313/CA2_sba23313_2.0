481823dff427f4f8c376bb69b9bc828862d62171
sba23313 <sba23313@student.cct.ie>
2024-03-08 23:53:22 +0000

    employeetest
    creating employee object, display names of employees with empNum >m, and array to store employee objects

--- src/employeetest/EmployeeTest.java
+++ src/employeetest/EmployeeTest.java
@@ -15,7 +15,23 @@ public class EmployeeTest {
      */
     // GitHub repo link: https://github.com
     public static void main(String[] args) {
-        
+    // Create Employee objects
+        Employee emp1 = new Employee("Joe Bloggs", "jb@gmail.com");
+        Employee emp2 = new Employee("Ann Banana", "ab@gmail.com");
+        Employee emp3 = new Employee("Tom Thumb", "tt@gmail.com");
+
+        // Array to store Employee objects
+        Employee[] projectGroup = {emp1, emp2, emp3};
+
+        // Print nextEmpNum
+        System.out.println("Next Employee Number: " + Employee.getNextEmpNum());
+
+        // Display names of employees with empNum > m
+        int m = 2; // Example value for m
+        for (Employee emp : projectGroup) {
+            if (emp.getEmpNum() > m) {
+                System.out.println(emp.getName());
+            }
+        }
     }
- 
 }
